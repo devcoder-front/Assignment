da9899058fd035126f6f2caa466e8018544a9bb8
Abhishek Sharma <abhisheks647@gmail.com>
2020-06-08 14:51:27 +0530

    adding assignment

---
+++ Assignment/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Our environmental Projects are providing many environmental benefits arround the world';
+  price = '$7.50/ton'
+  card_1_Title = 'kariba redd+ - Zimbabwe'
+  card_1_Heading = 'Reduced Emissions from Deforestation and Degradation'
+  card_1_Price = '957,842 lbs. offset'
+  card_1_desc = 'Protects forests and wildlife on the southern shores of Lake Kariba in Zimbabwe, forming a giant biodiversity corridor.'
+  card_2_Title = 'Wolf creek landfill - usa'
+  card_2_Heading = 'Landfill Methane Gas Capture'
+  card_2_Price = '2,005,214 lbs. offset'
+  card_2_desc = 'Landfill methane gas-to-energy facility that provides significant environmental benefits to the sorrounding area.'
+  card_3_Title = 'gaston countylandfill - usa'
+  card_3_Heading = 'Landfill Methane Gas Capture'
+  card_3_Price = '721,663 lbs. offset'
+  card_3_desc = 'Voluntary collection and destruction of landfill methane in two  open flares and three internal combustion engines.' 
+  card_4_Title = 'Natural pact land art - costarica'
+  card_4_Heading = 'Reforestation through Land Art'
+  card_4_Price = '327 lbs.offset'
+  card_4_desc = 'Unique "land art" reforestation and conservation  of forests that extend an important biological corridor for many endangered species.'
+  card_5_Title = 'arcata sunnybrae tract - usa'
+  card_5_Heading = 'Improved Forest Management'
+  card_5_Price = '710,093 lbs.offset'
+  card_5_desc = 'Preservation of large tracts of coastal Redwood and Doughals fir that were slated for harvesting by the timber company.'
+  card_6_Title = 'terrapass mixed portfolio - mixed'
+  card_6_Heading = 'Mixed'
+  card_6_Price = '241,967 lbs.offset'
+  card_6_desc  = 'Greenhouse gas reductions made at a variety of projects, including: reforestation, methane capture, and renewable energy.'
+  card_7_Title = 'myamyn conservation - australia'
+  card_7_Heading = 'Reforestation & Hydropower'
+  card_7_Price = '995,908 lbs.offset'
+  card_7_desc  = 'Supporting biodiversity and reforestation in Victoria, Australia; along with carbon reduction from a hydropower plant in Vietnam.'
+  price2 = '$7.50/ton'
+  card_8_Title = 'nature lab urban forestry - canada'
+  card_8_Heading = 'Urban Reforestation'
+  card_8_Price = '13,033 lbs.offset'
+  card_8_desc  = 'Rehabilitating degraded urban and sub-urban areas through reforestation, adding significant greenery and restoring biodiversity.'
+  price3 = '$22.50/ton'
+
+
+
+}
